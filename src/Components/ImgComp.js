c20f44a69dd1cd88adcccaff2354e519f4ce0b5e
Jeremiah Sifuentes <jerrysifuentes01@gmail.com>
2021-04-18 19:04:22 -0500

    Basic React ImgCarousel

---
+++ src/Components/ImgComp.js
@@ -0,0 +1,17 @@
+
+
+const ImgComp = ({src}) => {
+    
+    let imgStyles={
+        width: 100 + "%",
+        height: "auto"
+    }
+
+
+
+    return (
+        <img src={src} alt="slide-img" style={imgStyles}></img>
+    )
+}
+
+export default ImgComp
